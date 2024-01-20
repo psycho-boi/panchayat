cb9a6d8343a7d839a89fc0412bba98a7a5a2c5a5
jafar <jafarmulla107@gmail.com>
2024-01-21 00:49:40 +0530

    first commit

---
+++ resources/views/homapage.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <h2>THIS IS THE HOMEPAGE</h2>
+        <div class="hero-slide owl-carousel site-blocks-cover">
+           <div class="intro-section" style="background-image: url('{{asset('assets/images/hero_1.jpg')}}');">
+             <div class="container">
+               <div class="row align-items-center">
+                 <div class="col-lg-12 mx-auto text-center" data-aos="fade-up">
+                   <h1>Academics University</h1>
+                 </div>
+               </div>
+             </div>
+           </div>
+       
+           <div class="intro-section" style="background-image: url('{{asset('assets/images/+.jpg')}}');">
+             <div class="container">
+               <div class="row align-items-center">
+                 <div class="col-lg-12 mx-auto text-center" data-aos="fade-up">
+                   <h1>You Can Learn Anything</h1>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+
+
+         {{-- sarpanch section...  --}}
+         <div class="site-section" >
+          <div class="container">
+            <div class="row mb-5 justify-content-center text-center">
+              <div class="col-lg-4 mb-5">
+                <h2 class="section-title-underline mb-5">
+                  <span>Why Academics Works</span>
+                </h2>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+    
+                <div class="feature-1 ">
+                  {{-- <div class="icon-wrapper bg-primary">
+                    <span class="flaticon-mortarboard text-white"></span>
+                  </div> --}}
+                  <div class="feature-1-content">
+                    {{-- <h2>Personalize Learning</h2> --}}
+                    <img src="{{asset('assets/images/sarpanch.webp')}}" class="img-fluid rounded" alt="">
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-6 col-md-6 mb-4 mb-lg-0">
+                <div class="feature-1 ">
+                  {{-- <div class="icon-wrapper bg-primary">
+                    <span class="flaticon-school-material text-white"></span>
+                  </div> --}}
+                  <div class="feature-1-content">
+                    <h2>Sarpanch Message</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit morbi hendrerit elit</p>
+                  </div>
+                </div> 
+              </div>
+              <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                <div class="feature-1 ">
+                  {{-- <div class="icon-wrapper bg-primary">
+                    <span class="flaticon-library text-white"></span>
+                  </div> --}}
+                  <div class="feature-1-content align-items-center">
+                    <h2>Panch info</h2>
+                    <p><a href="#" class="btn btn-primary px-4 rounded-0">view</a></p>
+                  </div>
+                </div> 
+              </div>
+            </div>
+          </div>
+        </div>
+
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('assets/fonts/icomoon/style.css')}}">
+
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/jquery-ui.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/owl.theme.default.min.css')}}">
+
+    <link rel="stylesheet" href="{{asset('assets/css/jquery.fancybox.min.css')}}">
+
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap-datepicker.css')}}">
+
+    <link rel="stylesheet" href="{{asset('assets/fonts/flaticon/font/flaticon.css')}}">
+
+    <link rel="stylesheet" href="{{asset('assets/css/aos.css')}}">
+    <link href="{{asset('assets/css/jquery.mb.YTPlayer.min.css')}}" media="all" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+</head>
+<body>
+    
+    @yield('content')
+
+
+
+
+
+
+
+
+
+    
+
+    <script src="{{asset('assets/js/jquery-3.3.1.min.js')}}"></script>
+    <script src="{{asset('assets/js/jquery-migrate-3.0.1.min.js')}}"></script>
+    <script src="{{asset('assets/js/jquery-ui.js')}}"></script>
+    <script src="{{asset('assets/js/popper.min.js')}}"></script>
+    <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('assets/js/owl.carousel.min.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.stellar.min.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.countdown.min.js')}}"></script>
+    <script src="{{asset('assets/js/bootstrap-datepicker.min.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.easing.1.3.js')}}"></script>
+    <script src="{{asset('assets/js/aos.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.fancybox.min.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.sticky.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.mb.YTPlayer.min.js')}}"></script>
+    <script src="{{asset('assets/js/main.js')}}"></script>
+</body>
+</html>
